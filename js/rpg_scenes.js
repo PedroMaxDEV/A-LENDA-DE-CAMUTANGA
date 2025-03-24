de403e0b62d0dcc1031bb253de79262420a68879
PedroMaxDEV <irmao.max09@gmail.com>
2025-03-24 11:37:30 -0300

    aaaaaaaaaaaaaaaaaaaaa

--- js/rpg_scenes.js
+++ js/rpg_scenes.js
@@ -788,7 +788,7 @@ Scene_Map.prototype.isMenuEnabled = function() {
 };
 
 Scene_Map.prototype.isMenuCalled = function() {
-    return false;
+    return Input.isTriggered('menu') || TouchInput.isCancelled();
 };
 
 Scene_Map.prototype.callMenu = function() {
